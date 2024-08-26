cfb0c4c45edba45e961c4f92efc2bf59ea1b62ef
sony <sony@gmail.com>
2024-08-26 23:31:39 +0530

    additional learning on Aug26

--- cypressAutomation/cypress/integration/examples/Test1.js
+++ cypressAutomation/cypress/integration/examples/Test1.js
@@ -44,6 +44,29 @@ describe('Suite1',function(){
         //click add to cart button
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
         
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            const textVeg=$el.find('h4.product-name').text()
+            if(textVeg.includes("Cashews")){
+                cy.wrap($el).find('button').click()         //need to resolve $el promise with the help of cy.wrap
+            }
+        })
+
+
+        //const logo=cy.get('.brand')
+        //cy.log(logo.text())         // error: logo.text is not a function bcz cypress gets confuse once we declare 'const logo' as it is not a cypress command
+
+        const logo=cy.get('.brand').then(function(logoelement)          //so we have to handle/resolve the promise here manually
+        {
+            cy.log(logoelement.text())          
+        })
+        //cy.log(logo.text())
+        //cy.get('.brand').text()                 //text() is not a cypress command/method. text() is a jQuery method(supported by cypress)
+
+        console.log('Hey there')        // Non cypress command, asynchronous, prints the content in developer console log(onyl works for cypress tool)
+        // also need to resolve promise manually for the execution of printing 'Hey there' after a specfic step
+
+        //'difference b/w javascript console.log and cypress console.log is that it prints the content in terminal and developer tools console respectively'
 
     })
 })
